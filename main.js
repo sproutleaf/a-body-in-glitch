df9433a4dd1ae0e0ae23da018db271af7913f2ae
miaoye que <mq435@nyu.edu>
2023-11-20 00:50:05 -0500

    photo intervention

---
+++ main.js
@@ -0,0 +1,25 @@
+function blurry() {
+    $("#blurry").css("color", "white");
+    $("#blurry").css("text-shadow", "0 0 0px transparent");
+    $("#fragmented").css("pointer-events", "all");
+    $("#bp").css("visibility", "visible");
+}
+
+function fragmented() {
+    $("#fragmented").css("color", "white");
+    $("#fragmented").css("text-decoration", "none");
+    $("#disembodied").css("pointer-events", "all");
+    $("#fp").css("visibility", "visible");
+
+}
+
+function disembodied() {
+    $("#disembodied").css("color", "white");
+    $("#disembodied").css("text-shadow", "0 0 0px transparent");
+    $("#dp").css("visibility", "visible");
+    setTimeout(second, 2500);
+}
+
+function second() {
+    window.location.href = "miracle.html";
+}
